1ca33d2e7a534c1ca1e2d4722eeb1e91fe35e64b
Nichita <nichita.afanasiev@gmail.com>
2020-11-27 15:01:27 +0300

    Initial Commit

---
+++ Bank App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bank App
+//
+//  Created by Nichita Afanasiev on 27.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
